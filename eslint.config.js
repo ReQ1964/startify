3f85025cc372b1b0395ab018990b2614e7d64aa4
ReQ1964 <slomianykacper4@gmail.com>
2024-03-20 22:51:32 +0100

    feat: :sparkles: add basic layout, variables and astro - prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
 export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs['flat/recommended'],
   eslintConfigPrettier,
   {
