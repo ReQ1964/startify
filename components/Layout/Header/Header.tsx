d53e6dd167a701359bdb968e71211fb4a6091df9
ReQ1964 <slomianykacper4@gmail.com>
2024-03-23 23:27:21 +0100

    feat: :sparkles: add mobile header, install icons, add react - ts config and packages

---
+++ components/Layout/Header/Header.tsx
@@ -0,0 +1,50 @@
+import { useState, type ReactNode } from 'react';
+import classes from './Header.module.scss';
+import logo from '@/src/assets/logo.png';
+import menuClose from '@/src/assets/icons/menuClose.svg';
+import menuOpen from '@/src/assets/icons/menuOpen.svg';
+
+const Header = ({ links }: { links: ReactNode }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const handleMenuState = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.controls}>
+        <a href="/">
+          <img
+            className={classes.logo}
+            src={logo.src}
+            alt="HR Startify logo, click to navigate to homepage."
+          />
+        </a>
+        {isMenuOpen ? (
+          <img
+            className={classes.menuIcon}
+            src={menuClose.src}
+            alt="Click to close the navbar menu."
+            onClick={handleMenuState}
+          />
+        ) : (
+          <img
+            className={classes.menuIcon}
+            src={menuOpen.src}
+            alt="Click to open the navbar menu."
+            onClick={handleMenuState}
+          />
+        )}
+      </div>
+
+      <nav
+        className={`${classes.mobileNavbar} ${isMenuOpen ? classes.active : ''}`}
+      >
+        {links}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
